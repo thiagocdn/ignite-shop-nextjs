e4b9d71d06dcf910076b5a208e35587eaae1c8e2
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-07 19:30:25 -0300

    Created product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,31 @@
 import { useRouter } from 'next/router'
 
+import Image from 'next/image'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
 
-  console.log('query', query)
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image  /> */}</ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
 
-  return <h1>Product</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt beatae
+          recusandae libero, labore doloremque voluptate temporibus molestias
+          pariatur nobis quo dolor, ad, repudiandae debitis aliquam fuga ut
+          minus illum explicabo!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
