3f346d0879a77510636b9e3424b4117e64239e4d
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-08 21:16:19 -0300

    Created success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efeturada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Thiago Cedran</strong>, sua{' '}
+        <strong>Camiseta Blabla</strong> já está a caminho de sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
